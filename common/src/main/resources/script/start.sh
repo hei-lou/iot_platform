0dcad0e19118d669c3957f5f53295c49f1615454
ziven1024@gmail.com <ziven1024@gmail.com>
2021-04-08 10:45:24 +0800

    删除无用脚本

--- common/src/main/resources/script/start.sh
+++
@@ -1,8 +0,0 @@
-docker start redis
-docker start zookeeper
-
-nohup java -jar /app/jar/heilou-iot.jar > /dev/null 2>heilou-iot-error.log &
-nohup java -jar /app/jar/heilou-web.jar > /dev/null 2>heilou-web-error.log &
-
-
-docker exec -it zookeeper /bin/bash
